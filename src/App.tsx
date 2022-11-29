c663d95cbc5d46484352eb1677afdbfed8d066c3
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2022-11-29 23:42:17 +0000

    add and configure lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {ThemeProvider} from 'styled-components';
-import { Button } from "./components/Button";
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
@@ -12,7 +12,6 @@ export function App() {
       <Button variant="danger" />
       <Button />
       <GlobalStyle />
-  </ThemeProvider>
+    </ThemeProvider>
   )
 }
-
