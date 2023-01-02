c3a72a0cbbcd16bc57a6f2af755ca9c81054a945
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2023-01-02 21:15:29 +0000

    creating header container

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">timer</a>
+        <a href="">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
