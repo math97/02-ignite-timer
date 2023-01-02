f5852e405ec99935f305621b799af23123d91675
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2023-01-02 22:00:20 +0000

    create home and start styled

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,37 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+        </FormContainer>
+
+        <span>minutos.</span>
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
